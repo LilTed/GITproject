301562720d3d2f0325988d9578b9659488888ea7
oygen87 <green182day@hotmail.com>
2018-04-09 15:13:35 +0200

    fix html form to button

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let multi2 = $("#secondnumber").val();
 let multiResult;
 
 $("#multiply").click(function(){
-    multiResult = multi1 * multi2;
-    $("#result").val(multiResult);
+    multiResult = parseInt(multi1) * parseInt(multi2);
+    $("#result").text(multiResult);
+    console.log(multiResult);
 });
