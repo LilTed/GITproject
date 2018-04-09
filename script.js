2b2cb85487519eca569803f4b4564cdfaa9b800e
BNiklas <niklas.burman@hotmail.se>
2018-04-09 15:27:40 +0200

    Started add function

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-<script></script>
+document.getElementById("add").onclick = function(){
+    var a = parseInt(document.getElementById("firstNumber").value);
+    var b = parseInt(document.getElementByID("secondNumber").value);
+    add(a,b);
+
+}
+
+function add(a,b){
+    console.log(a+b);
+}
