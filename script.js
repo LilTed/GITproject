8933900d21f3765789ced758e05c914429cf7968
Elin Johansson <elin.johansson42@gmail.com>
2018-04-09 15:18:25 +0200

    Fixed some on divide

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 
 
 //Adding event listener to divide button
-$("multiply").on("click", function (e) {
-    let result = Number($("firstNumber").val())/Number($("secondNumber").val());
-    $("result").html(result);
+$("#divide").on("click", function (e) {
+    let result = Number($("#firstNumber").val())/Number($("#secondNumber").val());
+    $("#result").html(result);
+}
 
 let multi1 = $("#firstnumber").val();
 let multi2 = $("#secondnumber").val();
 let multiResult;
 
 $("#multiply").click(function(){
-<<<<<<< HEAD
+
     multiResult = multi1 * multi2;
     $("#result").val(multiResult);
 
-=======
+
     multiResult = parseInt(multi1) * parseInt(multi2);
     $("#result").text(multiResult);
     console.log(multiResult);
->>>>>>> eu
+
 });
