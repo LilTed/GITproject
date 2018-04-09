11241aede57a16a892a68e9da4bb4aac803de414
Lina Liljenfeldt <lina.liljenfeldt@gmail.com>
2018-04-09 15:12:17 +0200

    Add subtracting function to script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-<script></script>
+let subtract = document.querySelector("#subtract");
+let field1 = document.querySelector("#firstNumber");
+let field2 = document.querySelector("#secondNumber");
+let p = document.querySelector("#result");
+
+subtract.addEventListener("click", subtraction);
+
+function subtraction(){
+    let result = parseInt(field1.value - field2.value);
+    p.innertext = result;
+}
