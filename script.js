8a60a7da889675dd7629503dab4fad99221748c9
Elin Johansson <elin.johansson42@gmail.com>
2018-04-09 15:09:23 +0200

    Added eventlistener for division

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 <script></script>
+
+
+//Adding event listener to divide button
+$("multiply").on("click", function (e) {
+    let result = Number($("firstNumber").value)/Number($("secondNumber").value);
+    $("result").html(result);
+});
