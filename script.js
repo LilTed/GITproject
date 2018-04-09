8a2287d02357a4b0e5d319c567608123bb0a453f
oygen87 <green182day@hotmail.com>
2018-04-09 15:23:34 +0200

    whatttt eugen

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 
 
 //Adding event listener to divide button
-$("multiply").on("click", function (e) {
-    let result = Number($("firstNumber").value)/Number($("secondNumber").value);
-    $("result").html(result);
+//$("multiply").on("click", function (e) {
+  //  let result = Number($("firstNumber").value)/Number($("secondNumber").value);
+    //$("result").html(result);
 
+//}
+
+//////// EUGENS MULTIPLY //////
 let multi1 = $("#firstnumber").val();
 let multi2 = $("#secondnumber").val();
 let multiResult;
@@ -12,5 +15,5 @@ let multiResult;
 $("#multiply").click(function(){
     multiResult = multi1 * multi2;
     $("#result").val(multiResult);
-
+    console.log(multiResult);
 });
